56964f4e2f3ed154285f1104bf907eb75a3c9389
HDN <hdn@oporko.com>
2024-04-24 16:45:52 -0400

    Implement PathmapTile and complete load_pathmap method

--- bf1942/pathmap.py
+++ bf1942/pathmap.py
@@ -10,30 +10,48 @@ from struct import calcsize, unpack
 from pathlib import Path
 
 class PathmapHeader:
+    '''Header for a raw pathmap file.'''
+
     HEADER_FORMAT = '=iiiiii'
 
     def __init__(self, data):
         assert len(data) == 6
 
-        self.ln2TilesPerRow = data[0]
-        self.ln2TilesPerCol = data[1]
-        self.ln2TileRes = data[2]
+        self.ln2_tiles_per_row = data[0]
+        '''Number of tiles per row as a power of 2.'''
 
-        self.compLevel = data[3]
+        self.ln2_tiles_per_column = data[1]
+        '''Number of tiles per column as a power of 2.'''
+
+        self.ln2_tile_resolution = data[2]
+        '''Tile resolution as a power of 2.'''
+
+        self.compression_level = data[3]
         '''Compression level between 0-3 for land vehicles, 2-5 for sea.'''
 
-        self.isInfo = data[4]
+        self.is_info = data[4]
         '''Whether this is an info map or not, 1 - info, 0 - not info.'''
 
-        self.dataOffset = data[5]
+        self.data_offset = data[5]
         '''Number of DWORDs (32 bits) to seek after header to get to data start. Value must be 0 or 2.'''
 
-        self.tilesPerRow = None
-        self.tilesPerCol = None
-        self.tiles = None
-        self.rowsPerTile = None
-        self.bytesPerRow = None
-        self.bytesPerTile = None
+        self.tiles_per_row = None
+        '''Number of tiles per row.'''
+
+        self.tiles_per_column = None
+        '''Number of tiles per column.'''
+
+        self.tile_count = None
+        '''Total number of tiles.'''
+
+        self.rows_per_tile = None
+        '''Number of rows per tile.'''
+
+        self.bytes_per_row = None
+        '''Number of bytes per row.'''
+
+        self.bytes_per_tile = None
+        '''Number of bytes per tile.'''
 
         if self.is_valid():
             self._compute_derived()
@@ -41,28 +59,64 @@ class PathmapHeader:
     def is_valid(self):
         '''Check whether header appears to be valid.'''
 
-        return self.ln2TilesPerRow == self.ln2TilesPerCol and \
-            self.ln2TilesPerRow <= 8 and \
-            self.ln2TileRes >= 6 and \
-            self.ln2TileRes <= 12 and \
-            (self.isInfo == 0 or self.isInfo == 1) and \
-            (self.dataOffset == 0 or self.dataOffset == 2)
+        return self.ln2_tiles_per_row == self.ln2_tiles_per_column and \
+            self.ln2_tiles_per_row <= 8 and \
+            self.ln2_tile_resolution >= 6 and \
+            self.ln2_tile_resolution <= 12 and \
+            (self.is_info == 0 or self.is_info == 1) and \
+            (self.data_offset == 0 or self.data_offset == 2)
 
     def _compute_derived(self):
-        self.tilesPerRow = 1 << self.ln2TilesPerRow
-        self.tilesPerCol = 1 << self.ln2TilesPerCol
-        self.tiles = self.tilesPerRow * self.tilesPerCol
-        self.rowsPerTile = 1 << self.ln2TileRes - self.compLevel
-        self.bytesPerRow = self.rowsPerTile >> 3 - self.isInfo
-        self.bytesPerTile = self.rowsPerTile * self.bytesPerRow
+        self.tiles_per_row = 1 << self.ln2_tiles_per_row
+        self.tiles_per_column = 1 << self.ln2_tiles_per_column
+        self.tile_count = self.tiles_per_row * self.tiles_per_column
+        self.rows_per_tile = 1 << self.ln2_tile_resolution - self.compression_level
+        self.bytes_per_row = self.rows_per_tile >> 3 - self.is_info
+        self.bytes_per_tile = self.rows_per_tile * self.bytes_per_row
 
     @classmethod
     def from_file(cls, file):
+        '''Read header from a file.'''
+
         header_bytes = file.read(calcsize(cls.HEADER_FORMAT))
         header_data = unpack(cls.HEADER_FORMAT, header_bytes)
         return PathmapHeader(header_data)
 
+class PathmapTile:
+    '''Represents a tile within a raw pathmap file.'''
+
+    FLAG_MIXED = -1
+    FLAG_DOGO = 0
+    FLAG_NOGO = 1
+
+    def __init__(self, flag, data=None):
+        self.flag = flag
+        '''Flag indicating type of tile, one of FLAG_DOGO, FLAG_NOGO or FLAG_MIXED.'''
+
+        self.data = data
+        '''Tile data, size is determined by bytes_per_tile attribute of PathmapHeader, only set when flag is FLAG_MIXED.'''
+
+    @classmethod
+    def from_file(cls, file, bytes_per_tile):
+        '''Read tile data from a file.'''
+
+        flag = int.from_bytes(file.read(4), 'little', signed=True)
+
+        if flag not in [cls.FLAG_NOGO, cls.FLAG_DOGO, cls.FLAG_MIXED]:
+            raise ValueError(f'Invalid tile flag: {flag}')
+
+        if flag == cls.FLAG_MIXED:
+            data = file.read(bytes_per_tile)
+            if len(data) < bytes_per_tile:
+                raise ValueError('Invalid tile data length')
+
+            return PathmapTile(flag, data)
+        else:
+            return PathmapTile(flag)
+
 def load_pathmap(source_file):
+    '''Load a raw pathmap file.'''
+
     src_path = Path(source_file)
 
     assert src_path.is_file()
@@ -70,4 +124,17 @@ def load_pathmap(source_file):
     with open(src_path, 'rb') as file:
         header = PathmapHeader.from_file(file)
 
-    return header
+        if not header.is_valid():
+            raise ValueError('Invalid header')
+
+        file.seek(header.data_offset * 4, 1)
+
+        tiles = []
+        for _ in range(header.tile_count):
+            tiles.append(PathmapTile.from_file(file, header.bytes_per_tile))
+
+        # check we're at EOF
+        if file.read(1) != b'':
+            raise ValueError('Invalid data length')
+
+    return (header, tiles)
--- bf1942/tests/test_pathmapheader.py
+++ bf1942/tests/test_pathmapheader.py
@@ -7,71 +7,71 @@ class PathmapHeaderTest(unittest.TestCase):
     def test_init(self):
         pm = PathmapHeader((1, 2, 3, 4, 5, 6))
 
-        self.assertEqual(1, pm.ln2TilesPerRow)
-        self.assertEqual(2, pm.ln2TilesPerCol)
-        self.assertEqual(3, pm.ln2TileRes)
-        self.assertEqual(4, pm.compLevel)
-        self.assertEqual(5, pm.isInfo)
-        self.assertEqual(6, pm.dataOffset)
+        self.assertEqual(1, pm.ln2_tiles_per_row)
+        self.assertEqual(2, pm.ln2_tiles_per_column)
+        self.assertEqual(3, pm.ln2_tile_resolution)
+        self.assertEqual(4, pm.compression_level)
+        self.assertEqual(5, pm.is_info)
+        self.assertEqual(6, pm.data_offset)
 
     def test_derived(self):
         # 2048 level 0 map
         pm = PathmapHeader((5, 5, 6, 0, 0, 2))
 
-        self.assertEqual(32, pm.tilesPerRow)
-        self.assertEqual(32, pm.tilesPerCol)
-        self.assertEqual(1024, pm.tiles)
-        self.assertEqual(64, pm.rowsPerTile)
-        self.assertEqual(8, pm.bytesPerRow)
-        self.assertEqual(512, pm.bytesPerTile)
+        self.assertEqual(32, pm.tiles_per_row)
+        self.assertEqual(32, pm.tiles_per_column)
+        self.assertEqual(1024, pm.tile_count)
+        self.assertEqual(64, pm.rows_per_tile)
+        self.assertEqual(8, pm.bytes_per_row)
+        self.assertEqual(512, pm.bytes_per_tile)
 
         # 2048 level 1 map
         pm = PathmapHeader((4, 4, 7, 1, 0, 2))
 
-        self.assertEqual(16, pm.tilesPerRow)
-        self.assertEqual(16, pm.tilesPerCol)
-        self.assertEqual(256, pm.tiles)
-        self.assertEqual(64, pm.rowsPerTile)
-        self.assertEqual(8, pm.bytesPerRow)
-        self.assertEqual(512, pm.bytesPerTile)
+        self.assertEqual(16, pm.tiles_per_row)
+        self.assertEqual(16, pm.tiles_per_column)
+        self.assertEqual(256, pm.tile_count)
+        self.assertEqual(64, pm.rows_per_tile)
+        self.assertEqual(8, pm.bytes_per_row)
+        self.assertEqual(512, pm.bytes_per_tile)
 
         # 2048 info map
         pm = PathmapHeader((5, 5, 6, 1, 1, 2))
-        self.assertEqual(32, pm.tilesPerRow)
-        self.assertEqual(32, pm.tilesPerCol)
-        self.assertEqual(1024, pm.tiles)
-        self.assertEqual(32, pm.rowsPerTile)
-        self.assertEqual(8, pm.bytesPerRow)
-        self.assertEqual(256, pm.bytesPerTile)
+        self.assertEqual(32, pm.tiles_per_row)
+        self.assertEqual(32, pm.tiles_per_column)
+        self.assertEqual(1024, pm.tile_count)
+        self.assertEqual(32, pm.rows_per_tile)
+        self.assertEqual(8, pm.bytes_per_row)
+        self.assertEqual(256, pm.bytes_per_tile)
 
         # 4096 level 0 map
         pm = PathmapHeader((6, 6, 6, 0, 0, 2))
 
-        self.assertEqual(64, pm.tilesPerRow)
-        self.assertEqual(64, pm.tilesPerCol)
-        self.assertEqual(4096, pm.tiles)
-        self.assertEqual(64, pm.rowsPerTile)
-        self.assertEqual(8, pm.bytesPerRow)
-        self.assertEqual(512, pm.bytesPerTile)
+        self.assertEqual(64, pm.tiles_per_row)
+        self.assertEqual(64, pm.tiles_per_column)
+        self.assertEqual(4096, pm.tile_count)
+        self.assertEqual(64, pm.rows_per_tile)
+        self.assertEqual(8, pm.bytes_per_row)
+        self.assertEqual(512, pm.bytes_per_tile)
 
         # 4096 level 2 map
         pm = PathmapHeader((4, 4, 8, 2, 0, 2))
 
-        self.assertEqual(16, pm.tilesPerRow)
-        self.assertEqual(16, pm.tilesPerCol)
-        self.assertEqual(256, pm.tiles)
-        self.assertEqual(64, pm.rowsPerTile)
-        self.assertEqual(8, pm.bytesPerRow)
-        self.assertEqual(512, pm.bytesPerTile)
+        self.assertEqual(16, pm.tiles_per_row)
+        self.assertEqual(16, pm.tiles_per_column)
+        self.assertEqual(256, pm.tile_count)
+        self.assertEqual(64, pm.rows_per_tile)
+        self.assertEqual(8, pm.bytes_per_row)
+        self.assertEqual(512, pm.bytes_per_tile)
 
         # 4096 info map
         pm = PathmapHeader((6, 6, 6, 1, 1, 2))
-        self.assertEqual(64, pm.tilesPerRow)
-        self.assertEqual(64, pm.tilesPerCol)
-        self.assertEqual(4096, pm.tiles)
-        self.assertEqual(32, pm.rowsPerTile)
-        self.assertEqual(8, pm.bytesPerRow)
-        self.assertEqual(256, pm.bytesPerTile)
+        self.assertEqual(64, pm.tiles_per_row)
+        self.assertEqual(64, pm.tiles_per_column)
+        self.assertEqual(4096, pm.tile_count)
+        self.assertEqual(32, pm.rows_per_tile)
+        self.assertEqual(8, pm.bytes_per_row)
+        self.assertEqual(256, pm.bytes_per_tile)
 
     def test_from_file(self):
         data = pack(PathmapHeader.HEADER_FORMAT, 1, 2, 3, 4, 5, 6)
@@ -79,12 +79,12 @@ class PathmapHeaderTest(unittest.TestCase):
 
         pm = PathmapHeader.from_file(file)
 
-        self.assertEqual(1, pm.ln2TilesPerRow)
-        self.assertEqual(2, pm.ln2TilesPerCol)
-        self.assertEqual(3, pm.ln2TileRes)
-        self.assertEqual(4, pm.compLevel)
-        self.assertEqual(5, pm.isInfo)
-        self.assertEqual(6, pm.dataOffset)
+        self.assertEqual(1, pm.ln2_tiles_per_row)
+        self.assertEqual(2, pm.ln2_tiles_per_column)
+        self.assertEqual(3, pm.ln2_tile_resolution)
+        self.assertEqual(4, pm.compression_level)
+        self.assertEqual(5, pm.is_info)
+        self.assertEqual(6, pm.data_offset)
 
     def test_is_valid(self):
         # valid data
